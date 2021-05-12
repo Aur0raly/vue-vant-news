5bd92087489a7a1636688b7f2ed2215b226c65d8
Aur0raly <twilightSMC@Gmail.com>
2021-05-12 11:08:45 +0800

    项目初始化/调整目录结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式表
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例，导入组件
 new Vue({
   router,
   store,
